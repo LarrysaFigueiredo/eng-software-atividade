38a4c1e0bdb4de78d7ea49f2567e3747af897648
luizjunior8 <71145941+luizjunior8@users.noreply.github.com>
2022-05-24 15:25:03 -0300

    Branch Junior

--- src/br/ufpb/dcx/eng/atv/pratica/App.java
+++ src/br/ufpb/dcx/eng/atv/pratica/App.java
@@ -1,6 +1,9 @@
 package br.ufpb.dcx.eng.atv.pratica;
 
 public class App {
-        
+    
+    public static void public static void main(String[] args) {
+        System.out.println("Starting new project");
+    }
 
 }

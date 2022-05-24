921ddedccfea8c957719536fb7ed81456175a741
luizjunior8 <71145941+luizjunior8@users.noreply.github.com>
2022-05-24 15:56:11 -0300

    Tem alguma coisa errada aqui

---
+++ package/atvJunior.java
@@ -0,0 +1,9 @@
+public class AppJunior {
+    
+    public static void main(String[] args) {
+        System.out.println("Starting new project");
+
+        System.out.println("Testando de novo, pq o vs ta brincando comigo");
+    }
+
+}
---
+++ src/br/ufpb/dcx/eng/atv/pratica/AppJunior.java
@@ -0,0 +1,10 @@
+
+public class AppJunior {
+    
+    public static void main(String[] args) {
+        System.out.println("Starting new project");
+
+        System.out.println("Testando de novo, pq o vs ta brincando comigo");
+    }
+
+}

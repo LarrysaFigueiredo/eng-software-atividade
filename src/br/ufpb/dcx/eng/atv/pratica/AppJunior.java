b778f0db716538be0271a7dbe6490396593f60c4
luizjunior8 <71145941+luizjunior8@users.noreply.github.com>
2022-05-24 15:50:28 -0300

    Só mais uma tentativa, agora vai

--- src/br/ufpb/dcx/eng/atv/pratica/AppJunior.java
+++ src/br/ufpb/dcx/eng/atv/pratica/AppJunior.java
@@ -1,8 +1,8 @@
 package br.ufpb.dcx.eng.atv.pratica;
 
-public class App {
+public class AppJunior {
     
-    public static void public static void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Starting new project");
 
         System.out.println("Testando de novo, pq o vs ta brincando comigo");
